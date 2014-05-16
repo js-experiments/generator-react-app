16f2d94d6f3d0ac1f0e96a881665b03d319fd783
k33g <ph.charriere@gmail.com>
2014-05-16 17:49:07 +0200

    #1

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,17 +1,17 @@
 /*=== Main Application ===*/
-var args = process.argv.splice(2);
-var express = require('express');
-
-var app = express()
+var express = require('express')
+  , http = require('http')
+  , bodyParser = require('body-parser')
+  , app = express()
+  , http_port = 3000
+  , args = process.argv.splice(2);
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded());
 
 require("./db/db").once('open', function callback () {
 	/*=== insert routes here ===*/
 	require("./all.routes")(app);
 });
 
-app.listen(args[0] || 3000);
-console.log("Listening on 3000")
+app.listen(args[0] || http_port);
+console.log("Listening on " + http_port);
